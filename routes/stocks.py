513e6f1e64905298e2d50ca96afaada09f49cd77
Gabriel Henriques <henriques.gabriel@outlook.com>
2021-02-13 22:54:37 -0300

    feat: new route (Forex) / fix: fixing random errors, installing gunicorn

--- routes/stocks.py
+++ routes/stocks.py
@@ -12,6 +12,10 @@ def show_stock_price(ticker):
     r = requests.get(string)
     if r.status_code == requests.codes.ok:
         return r.json(), 200
+    elif r.status_code == requests.codes.not_found:
+        return jsonify({'message': 'stock not found'}), 404
+    else:
+        return jsonify({'message': 'Internal server error'}), 500
 
 @app.route('/stocks/news/<ticker>')
 def show_stock_news(ticker):
@@ -20,6 +24,10 @@ def show_stock_news(ticker):
     r = requests.get(string)
     if r.status_code == requests.codes.ok:
         return r.json(), 200
+    elif r.status_code == requests.codes.not_found:
+        return jsonify({'message': 'stock not found'}), 404
+    else:
+        return jsonify({'message': 'Internal server error'}), 500
 
 @app.route('/stocks/profile/<ticker>')
 def show_profile(ticker):
@@ -29,6 +37,18 @@ def show_profile(ticker):
     r = requests.get(string)
     if r.status_code == requests.codes.ok:
         return r.json()
+    elif r.status_code == requests.codes.not_found:
+        return jsonify({'message': 'stock not found'}), 404
     else:
-        return jsonify({'message': 'Unknown error'}), 500
-    
+        return jsonify({'message': 'Internal server error'}), 500
+@app.route('/forex/rates/<currency>')
+def show_currency_price(currency):
+    currency_ticker = escape(currency)
+    string = '{}/forex/rates?base={}&token={}'.format(app.config["API_URL"],currency_ticker,app.config['API_KEY'])
+    r = requests.get(string)
+    if r.status_code == requests.codes.ok:
+        return r.json(), 200
+    elif r.status_code == requests.codes.not_found:
+        return jsonify({'message': 'forex not found'}), 404
+    else:
+        return jsonify({'message': 'Internal server error'}), 500
